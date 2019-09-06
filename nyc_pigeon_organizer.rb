8dea678d6df99bd38aaa552b9b63b75cae8f908d
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-06 15:36:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
 organized_hash = {}
 
 data.each do |color_gender_lives, attribute| 
-  values.each do |attribute, names|
+  attribute.each do |attribute, names|
     names.each do |name| 
       if organized_hash[name] = nil 
         organized_hash[name] = {}
