0c0ca88d3674e41203b3b95928e47e1a856703c2
Jonathan Tan <jonathantan@brandeis.edu>
2019-09-06 15:41:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,11 +6,6 @@ def nyc_pigeon_organizer(data)
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
         end
-        if pigeon_list[name][color_gender_lives] == nil
-          pigeon_list[name][color_gender_lives] = []
-        end
-        pigeon_list[name][color_gender_lives].push(stats.to_s)
-      end
     end 
   end
   pigeon_list
